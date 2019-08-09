a9ee8edb06b069c11948789c908fb0e05b3d5532
BurriTaco <jc.johncuison1@gmail.com>
2019-08-09 18:16:32 +0800

    Created Functions
    Yeah. Added functions to clean up a lot of the image embed commands. Added another usable prefix. Added some more NSFW commands and SFW commands.
    
    Code should be less of a headache to read now
    
    Also, it's not playing a game anymore; it's twitch streaming. And yes, the twitch channel is legit.

--- battle_bunny_bot.py
+++ battle_bunny_bot.py
@@ -1,8 +1,12 @@
 
 
-
+#Special thanks to the lads of the Tortoise Discord Bot Community for helping me create this wonderful piece of work. Check out their discord server: https://discord.gg/BSBMQ2n
+#They are an awesome lot.
+#Also special thanks to ShiftyWizard#4823 for teaching me Regex to allow nHentai Linker Function to work.
 #imports
 
+
+#Imports go here
 import json
 import re
 import asyncio
@@ -19,15 +23,18 @@ os.chdir(dname)
 
 #prefix for core commands
 prefix = "m!"
+prefix_2 = "momo"
 
 #Set To False To allow bot to work outside of nsfw channels
 n_universal = False
+
 #Set to False to prevent bot from responding to basic messages
 res_func = True
+
 #Set to False if you don't want the other NSFW commands to work in DMs (Not Recommended)
 nsfw_dm = True
 
-#redgex scanner thingy for 6 digiTS
+#redgex scanner for the holy 6 digits
 regular = r"(?:\D+?|^)(\d{6})(?=\D+?|$)"
 compiled = re.compile(regular)
 
@@ -40,8 +47,33 @@ with open("reddit_id.txt", "r") as f:
     with open("reddit_secret.txt", "r") as g:
         reddit = praw.Reddit(client_id=f.read(), client_secret=g.read(), user_agent='<Universal>:<Br12Ir5JUAlZSQ>:<v1> (by /u/WinterAmoeba514)')
 
+def subreddit_image_embedder(subreddit, filter='hot'):
+    subred = reddit.subreddit(subreddit)
+    
+    if filter == "hot":
+        neewmeem = subred.hot(limit=50)
+    elif filter == "top":
+        neewmeem = subred.top('all', limit=50)
+    elif filter == "new":
 
+        neewmeem = subred.new(limit=50)
+    lstmeem = list(neewmeem)
+    randsub = random.choice(lstmeem)
+    embed = discord.Embed(title=randsub.title,
+                       description=f'',
+                       url=randsub.url, colour=0x3498d)
+    embed.set_image(url=randsub.url)
+    return embed
 
+def momo_self(m):
+    return m.author == client.user
+
+def bot_info():
+    bot_info_embed = discord.Embed(title="Battle Bunny Info Card", url="https://github.com/Burri-Taco/Momo_Battle_Bunny",
+                          description="Made by @ShiftyWizard#4823 & @_BurriTaco_#0889 because they'd rather do Python than VB.", color=0xfabcbd)
+    bot_info_embed.set_thumbnail(url="https://i.redd.it/27bp6h2z55011.jpg")
+    bot_info_embed.set_footer(text="(The code is like, really really bad)")
+    return bot_info_embed
 
 #Startup Process
 @client.event
@@ -50,7 +82,8 @@ async def on_ready():
     print("Watashi ga kita!")
 
     #The displayed game of the bot
-    game = discord.Game("Usagi Simulator")
+    game = discord.Streaming(name="TED Talk: Bunny Girls, the Next Stage of Human Evolution",
+                             url="https://www.twitch.tv/momothebattlebunny")
     await client.change_presence(status=discord.Status.online, activity=game)
 
     #This is the startup message, copy the channel ID for the channel where you want the bootup message to go to.
@@ -62,32 +95,25 @@ async def on_ready():
         for guild in client.guilds: 
             for channel in guild.channels:
                 if channel.id in channels:
-                    await channel.send("🥕Moshi moshi. Batorubanī wa koko ni arimasu!🥕 \n 🥕Do m!help for available commands🥕")
+                    await channel.send("🥕Moshi moshi🥕 \n Batorubanī wa koko ni arimasu! \n Do m!help for available commands")
 
+#This bit prevents multiple messages being sent if the same person shares more than 1 server with the bot
+#Put discord ID on the file if u want to DM people when the bot startsup
     with open("user_id_list.txt", "r") as f:
         message_user_id = f.readlines()
         message_user_id = [int(x.strip()) for x in message_user_id]
-        
         snakecaseboyimdown = []
-
-
         for guild in client.guilds:
             for user in guild.members:
                 if user.id in message_user_id:
                     if not user in snakecaseboyimdown:
                         snakecaseboyimdown.append(user)
-
-
+        #The message it sends on startup
         for user in snakecaseboyimdown:
-            await user.send("🥕Yo! someone turned me on, just letting you know.🥕")
+            await user.send("Yo! someone turned me on, just letting you know.")
             await asyncio.sleep(2)
-            await user.send("🥕To stop receiving these messagers, type 'STOP'🥕")
-            await asyncio.sleep(2)
-            await user.send("🥕I'm kidding, that actually doesn't work. You'd need to message my Devs to remove you from the list.🥕")
-
-
+            await user.send("To stop receiving these messagers, type 'STOP'")
             
-
 @client.event   
 async def on_message(message):
 
@@ -100,182 +126,72 @@ async def on_message(message):
 
     
     #commands that are only triggered if the message starts with the prefix stated before
-    if message.content.startswith(prefix):
+    if message.content.startswith(prefix) or message.content.startswith(prefix_2):
         if "help" in message.content.lower() or "info" in message.content.lower():
-            embed = discord.Embed(title="Battle Bunny Info Card", url="https://github.com/Burri-Taco/Momo_Battle_Bunny",
-                                  description="Made by @ShiftyWizard#4823 & @_BurriTaco_#0889 because they'd rather do Python than VB.", color=0xfabcbd)
-            embed.set_thumbnail(url="https://i.redd.it/27bp6h2z55011.jpg")
-            embed.set_footer(text="(The code is like, really really bad)")
-            await message.channel.send(embed=embed)
+            await message.channel.send(embed=bot_info())
 
-        #Shitty Clear Chat Functions    
-        elif "clear" in message.content.lower():
-            await message.channel.send("Not Clearing Messages \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n  \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n Cleared Messages")
+        #Sh*tty Clear Chat Functions    
+        elif "purge" in message.content.lower() or message.channel.is_nsfw:
+            if (message.channel.type == discord.DMChannel):
+                await message.channel.send("Sorry, that command is only available in server channels")
+            elif "bot" in message.content.lower():
+                num_self_purge_amount = message.content.split(' ')[2]
+                to_be_deleted = await message.channel.purge(limit=int(num_self_purge_amount), check=momo_self)
+                await message.channel.send('I Deleted {} message(s) from me'.format(len(to_be_deleted)))
+            elif "msg" in message.content.lower():
+                num_msg_purge_amount = message.content.split(' ')[2]
+                deleted = await message.channel.purge(limit=int(num_msg_purge_amount))
+                await message.channel.send('I Deleted {} message(s)'.format(len(deleted)))
+            else:
+                await message.channel.send("What? That's not how you do it. \n ```m!purge msg/bot <number> \n``` is the correct usage")
 
-        #This bit was made possible by Fwiz and Hideki Ryuga and their Tortoise Discord Bot (https://github.com/fwizzz/Tortoise-Discord-Bot)
-
+        #Subreddit image poster bit
         elif "kizuna" in message.content.lower() or "kzn" in message.content.lower():
-                subred = reddit.subreddit('KizunaA_Irl')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
+            await message.channel.send(embed=subreddit_image_embedder('kizunA_Irl'))
+
+        elif "anime_irl" in message.content.lower():
+            await message.channel.send(embed=subreddit_image_embedder('anime_irl'))
 
         elif "wholesomeanimeme" in message.content.lower():
-                subred = reddit.subreddit('wholesomeanimemes')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
+            await message.channel.send(embed=subreddit_image_embedder('wholesomeanimemes'))
 
         elif "animeme" in message.content.lower():
-                subred = reddit.subreddit('animemes')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
+            await message.channel.send(embed=subreddit_image_embedder('animemes'))
+
         elif "wholesome" in message.content.lower():
-                subred = reddit.subreddit('wholesome')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
+            await message.channel.send(embed=subreddit_image_embedder('wholesome'))
 
-        elif "meme" in message.content.lower():
-                subred = reddit.subreddit('memes')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
+        elif "reddit" in message.content.lower():
+            sub = message.content.split(' ')[1]
+            await message.channel.send(embed=subreddit_image_embedder(sub))
+        elif "kemonomimi" in message.content.lower():
+            await message.channel.send(embed=subreddit_image_embedder('kemonomimi'))
+            
 
-        #nsfw command
-        elif "hentai gif" in message.content.lower():
-            #checks if command was run in NSFW channel
-            if nsfw_dm is True:
-                subred = reddit.subreddit('hentai_GIF')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
-            elif message.channel.is_nsfw():
-                subred = reddit.subreddit('hentai_GIF')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
-
-        elif "hentai" in message.content.lower():
-            #checks if command was run in NSFW channel
-            if nsfw_dm is True:
-                subred = reddit.subreddit('hentai') 
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
-            elif message.channel.is_nsfw():
-                subred = reddit.subreddit('hentai')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
-
-        elif "rule34 gif" in message.content.lower() or "r34 g" in message.content.lower():
-            #checks if command was run in NSFW channel
-            if nsfw_dm is True:
-                subred = reddit.subreddit('rule34gifs')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
-            elif message.channel.is_nsfw():
-                subred = reddit.subreddit('rule34gifs')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
-
-        elif "rule34" in message.content.lower() or "r34" in message.content.lower():
-            #checks if command was run in NSFW channel
-            if nsfw_dm is True:
-                subred = reddit.subreddit('rule34')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
-            elif message.channel.is_nsfw():
-                subred = reddit.subreddit('rule34')
-                neewmeem = subred.top(limit=100)
-                lstmeem = list(neewmeem)
-                randsub = random.choice(lstmeem)
-                embed = discord.Embed(title=randsub.title,
-                                      description=f'',
-                                      url=randsub.url, colour=0x3498d)
-                embed.set_image(url=randsub.url)
-                await message.channel.send(embed=embed)
-
-            else:
-                #return if it isn't
-                await message.channel.send("🥕Sorry, that command is only available to be used in NSFW channels.🥕")
+        #nsfw commands
+        #checks if command was run in NSFW channel
+        elif (message.channel.type != discord.DMChannel) or message.channel.is_nsfw:
+            if "hentai_irl" in message.content.lower():
+                await message.channel.send(embed=subreddit_image_embedder('hentai_irl'))
+            elif "hentai_gif" or "animated hentai" in message.content.lower():
+                await message.channel.send(embed=subreddit_image_embedder('hentai_GIF'))
+            elif "rule34 gif" or "r34 g" in message.content.lower():
+                await message.channel.send(embed=subreddit_image_embedder('rule34'))
+            elif "rule34" or "r34" in message.channel.lower():
+                await message.channel.send(embed=subreddit_image_embedder('rule34gifs'))
+            elif "hentai" in message.channel.lower():
+                await message.channel.send(embed=subreddit_image_embedder('hentai'))    
+            elif "kitsunemimi" in message.channel.lower():
+                await message.channel.send(embed=subreddit_image_embedder('kitsunemimi'))
 
 
          #If Command was not recognised       
         else:
-            await message.channel.send("🥕Unknown command, you can enable/disable some commands by editting the python file I use to work.🥕")
+            await message.channel.send("Unknown command, you can enable/disable some commands by editting the python file I use to work.")
             await message.channel.send("You can check out the available commands by checking out my GitHub Page, which can be found here:")
+            await message.channel.send(embed=bot_info())
 
-            embed = discord.Embed(title="Battle Bunny Info Card", url="https://github.com/Burri-Taco/Momo_Battle_Bunny",
-                                  description="Made by @ShiftyWizard#4823 & @_BurriTaco_#0889 because they'd rather do Python than VB.", color=0xfabcbd)
-            embed.set_thumbnail(url="https://i.redd.it/27bp6h2z55011.jpg")
-            embed.set_footer(text="(The code is like, really really bad)")
-            await message.channel.send(embed=embed)
+            
 
 
     #this bit makes bot respond to good bot and bad bot
@@ -287,6 +203,7 @@ async def on_message(message):
         else:
             await message.channel.send("Hai, Batorubanī desu")
 
+    #Responses to basic messages
     elif res_func is True:
         if "oh no" in message.content.lower():
             await message.channel.send("Oh Yes U(ㅇㅅㅇ❀)U")
@@ -299,14 +216,16 @@ async def on_message(message):
             embed.set_image(
                 url="https://www.myinstants.com/media/instants_images/it-was-me-dio.jpg")
             await message.channel.send(embed=embed)
-
-            
-    elif "stop" in message.content.lower():
-        await message.channel.send("🥕lol no🥕")
-        await asyncio.sleep(2)
-        await message.channel.send("🥕Actually, let me see what I can do🥕")
-        await asyncio.sleep(2)
-        await message.channel.send("🥕Stopped just for you :heart:🥕")
+        
+        #Suprisiingly this bit is only meant to work in DM's, when someone responds to the startup messages at the start. I'll eventually fix it up
+        elif "stop" in message.content.lower():
+            await message.channel.send("🥕lol no🥕")
+            await asyncio.sleep(2)
+            await message.channel.send("🥕Actually, let me see what I can do🥕")
+            await asyncio.sleep(2)
+            await message.channel.send("🥕Stopped just for you :heart:🥕")
+            await asyncio.sleep(2)
+            await message.channel.send("🥕Kidding, that doesn't actually work. You'd need to message my Devs. Sorry🥕")
 
 
 
@@ -318,12 +237,8 @@ async def on_message(message):
             nlinks.append(nurl.format(num))
             await message.channel.send("\n".join(nlinks))
 
-
-
-
-
-
 #Bot Token Goes Here
 #The Bot Token Was put in a text file, you may/may not need to create a new .txt file and put it there.
 with open("server_key.txt", "r") as f:
     client.run(f.read())
+    
