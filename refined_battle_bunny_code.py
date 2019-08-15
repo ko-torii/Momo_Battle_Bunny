b8a6a98653d432c6a08c890b2174c3e5be14bcf6
BurriTaco <jc.johncuison1@gmail.com>
2019-08-15 21:53:46 +0800

    Cogs and Classes
    Just to sort out the commands really, for the purpose of making the m!help command neater

--- refined_battle_bunny_code.py
+++ refined_battle_bunny_code.py
@@ -44,41 +44,60 @@ async def on_ready():
     CurrentStatus = "TED Talk: Catgirls, the Next Stage Of Human Evolution"
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=CurrentStatus))
 
-@bot.command()
-async def doujin(ctx, *ids):
-    '''Generates a nHentai.net link. Usage: m!doujin <Doujin ID>'''
-    nIDs = ids
-    nlinks = []
-    nurl = "https://www.nhentai.net/g/{0}"
-    if (str(ctx.channel.type) == "private") or (ctx.channel.is_nsfw()):
-        for nID in nIDs:
-            nlinks.append(nurl.format(nID))
-            await ctx.send("\n".join(nlinks))
-    else:
-        await ctx.send("Buddy, you can only use this command in NSFW channels")
 
-@bot.command(name="reddit")
-async def subreddit_spooky(ctx,sub):    
-    '''Posts a Hot post from a subreddit. Usage: $reddit <subreddit>'''
-    reddit_subreddit = reddit.subreddit(sub)
-    if not reddit_subreddit.over18:
-        await ctx.send(embed=subreddit_image_embedder(sub))
-    elif reddit_subreddit.over18 and (str(ctx.channel.type) == "private") or (not ctx.channel.is_nsfw()):
-        await ctx.send("Sorry, you can only access that command in NSFW channels in servers.")
-    elif reddit_subreddit.over18 and ctx.channel.is_nsfw():
-        await ctx.send(embed=subreddit_image_embedder(sub))
-    
-@bot.command()
-async def aname(ctx,args):
-    '''A Name'''
-    await ctx.send(args)
-    print(args)
+class SFW(commands.Cog):
+    @commands.command(name="reddit")
+    async def subreddit_spooky(self, ctx,sub):    
+        '''A hotpost from a specified subreddit. Usage: m!reddit <subreddit>'''
+        reddit_subreddit = reddit.subreddit(sub)
+        if not reddit_subreddit.over18:
+            await ctx.send(embed=subreddit_image_embedder(sub))
+        elif reddit_subreddit.over18 and (str(ctx.channel.type) == "private") or (not ctx.channel.is_nsfw()):
+            await ctx.send("Sorry, you can only access that command in NSFW channels in servers.")
+        elif reddit_subreddit.over18 and ctx.channel.is_nsfw():
+            await ctx.send(embed=subreddit_image_embedder(sub))
+
+    @commands.command()
+    async def info(self, ctx):
+        '''Shows the GitHub repo'''
+        await ctx.send("Here's a link to my GitHub Repo")
+        bot_info_embed = discord.Embed(title="Battle Bunny Info Card", url="https://github.com/Burri-Taco/Momo_Battle_Bunny",
+                                    description="Made by @ShiftyWizard#4823 & @_BurriTaco_#0889 because they'd rather do Python than VB.", color=0xfabcbd)
+        bot_info_embed.set_thumbnail(url="https://i.redd.it/27bp6h2z55011.jpg")
+        bot_info_embed.set_footer(text="(The code is like, really really bad)")
+        await ctx.send(embed= bot_info_embed)
+        await ctx.send("For a list of commands, do m!help")
+        
+    @commands.command()
+    async def aname(self,ctx,args):
+        '''A Name'''
+        await ctx.send(args)
+        print(args)
 
 
-@bot.command()
-async def test(ctx):
-    '''States The Channel Type'''
-    await ctx.send(ctx.channel.type)
+    @commands.command()
+    async def test(self, ctx):
+        '''States The Channel Type'''
+        await ctx.send(ctx.channel.type)
+
+
+class NSFW(commands.Cog):
+    @commands.command()
+    async def doujin(self, ctx, *ids):
+        '''Generates a nHentai.net link. Usage: m!doujin <Doujin ID>'''
+        nIDs = ids
+        nlinks = []
+        nurl = "https://www.nhentai.net/g/{0}"
+        if (str(ctx.channel.type) == "private") or (ctx.channel.is_nsfw()):
+            for nID in nIDs:
+                nlinks.append(nurl.format(nID))
+                await ctx.send("\n".join(nlinks))
+        else:
+            await ctx.send("Buddy, you can only use this command in NSFW channels")
+
+
+bot.add_cog(SFW(bot))
+bot.add_cog(NSFW(bot))
 
 #Bot's Token
 with open("server_key.txt", "r") as f:
